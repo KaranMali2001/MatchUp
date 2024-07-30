b7eb31a6099cf2ce7fcfb8b9a2375a11e76079bc
karan <karanmali122001@gmail.com>
2024-07-30 14:26:59 +0530

    neonDB

--- database/connection.go
+++ database/connection.go
@@ -15,8 +15,8 @@ func init() {
 	//conn := "postgresql://postgres:password@postgres:5432/MatchUp"
 
 	//	conn := "postgresql://postgres:password@localhost:5432/MatchUp"
-	conn:="postgres://avnadmin:AVNS_3Hp-HoMwD06UlWXBfv-@pg-1ed982b1-karansignup5599.f.aivencloud.com:13094/defaultdb?sslmode=require"
-	
+	conn:="postgresql://MatchUp_owner:KSNoaf0P9HqT@ep-mute-meadow-a1s2qyo6.ap-southeast-1.aws.neon.tech/MatchUp?sslmode=require"
+
 	//docker run -e POSTGRES_PASSWORD=password -e POSTGRES_DB=MatchUp -v MatchUpDB:/var/lib/postgresql/data -p 5432:5432 -d postgres
 
 	Db, err = gorm.Open(postgres.Open(conn), &gorm.Config{})
