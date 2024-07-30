b9fc200cc7ff098411251bc6a1bcfea08f655cba
karan <karanmali122001@gmail.com>
2024-07-30 14:06:54 +0530

    avianDB

--- database/connection.go
+++ database/connection.go
@@ -13,8 +13,11 @@ var Db *gorm.DB
 func init() {
 	var err error
 	//conn := "postgresql://postgres:password@postgres:5432/MatchUp"
-		conn := "postgresql://postgres:password@localhost:5432/MatchUp"
-		//docker run -e POSTGRES_PASSWORD=password -e POSTGRES_DB=MatchUp -v MatchUpDB:/var/lib/postgresql/data -p 5432:5432 -d postgres
+
+	//	conn := "postgresql://postgres:password@localhost:5432/MatchUp"
+	conn:="postgres://avnadmin:AVNS_3Hp-HoMwD06UlWXBfv-@pg-1ed982b1-karansignup5599.f.aivencloud.com:13094/defaultdb?sslmode=require"
+	
+	//docker run -e POSTGRES_PASSWORD=password -e POSTGRES_DB=MatchUp -v MatchUpDB:/var/lib/postgresql/data -p 5432:5432 -d postgres
 
 	Db, err = gorm.Open(postgres.Open(conn), &gorm.Config{})
 	if err != nil {
